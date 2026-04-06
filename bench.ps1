85d6411e0bd11e62fc611f54f080edcc33efd95c
Blackwood416 <animeweaver959@gmail.com>
2026-04-06 16:06:38 +0800

    Add bench.ps1 to run benchmark, edit run.ps1 to match new -m commandline args

--- bench.ps1
+++ bench.ps1
@@ -1,4 +1,4 @@
 cmd /c '"C:\Program Files (x86)\Intel\oneAPI\setvars.bat" && set' | ForEach-Object { if ($_ -match '^([^=]+)=(.*)$') { [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2], 'Process') } }; Write-Host ':: oneAPI environment initialized ::' -ForegroundColor Green
 cd e:\RiderProjects\Aila\build
-.\Aila.exe -m ..\Qwen3-0.6B | Tee-Object -FilePath "e:\RiderProjects\Aila\run_log.txt"
+.\Aila.exe -m ..\Qwen3-0.6B --bench --bench-pp 512 --bench-tg 512 | Tee-Object -FilePath "e:\RiderProjects\Aila\bench_log.txt"
 cd e:\RiderProjects\Aila
